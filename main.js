41739f91bed92e210b5ea65ab72bb4a2cc6ec79b
Morgan Fox <morganfox61297@gmail.com>
2019-05-15 10:03:35 -0400

    form fade

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ div1.classList.remove("hidden")
 div1.classList.add("show")
 }
 })
+
+let form = document.getElementById("form")
+window.addEventListener("scroll", function(){
+console.log(window.scrollY)
+if(window.scrollY > 200) {
+form.classList.remove("hidden")
+form.classList.add("show")
+}
+})
